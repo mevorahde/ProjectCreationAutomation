925d70c06eba62b48895025b73920e47211497a9
David Mevorah <mevorahde@hotmail.com>
2020-06-26 10:13:02 -0700

    initial commit

--- batch/create.bat
+++ batch/create.bat
@@ -1,18 +1,18 @@
 :: ENTER PATH WHERE THE script.py IS LOCATED.
-cd <PATH>
+cd C:\Users\David E\version-control\ProjectCreationAutomation
 
 python script.py %1 %2 %3
 
 :: ENTER PATH WHERE YOUR PROJECTS ARE SAVED
 :: e.g. C:\Users\<USERNAME>\Documents\Projects
-cd <PATH>\%1
+cd C:\Users\David E\version-control\%1
 
 
 git init
 
 ::ENTER YOUR GIT USERNAME
 git remote rm origin
-git remote add origin https://github.com/<USERNAME>/%1.git
+git remote add origin https://github.com/mevorahde/%1.git
 
 echo # %1 > README.md
 git add .
@@ -30,3 +30,6 @@ IF %3==sublime (subl .)
 
 :: OPENS VISUAL STUDIO CODE
 IF %3==vsc (code .)
+
+:: OPENS VISUAL STUDIO CODE
+IF %3== [](CONTINUE)
