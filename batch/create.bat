7c302ec0de63f9cd4b87890e2d0156d105eaaab4
David Mevorah <mevorahde@hotmail.com>
2020-06-26 12:22:08 -0700

    initial commit

--- batch/create.bat
+++ batch/create.bat
@@ -20,7 +20,7 @@ git commit -m "initial commit"
 git push -u origin master
 
 :: NO PARM TO OPEN IDE
-IF %3 =="" (echo Finished)
+IF %3 =="" (echo : Finished)
 
 :: OPENS PYCHARM
 IF %~3==pycharm (pycharm64 .)
