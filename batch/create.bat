0eb2175fe0d3ed662dd38c802b3bd38fe234aeba
David Mevorah <mevorahde@hotmail.com>
2022-05-14 05:49:18 -0700

    Add branch to set functionality of connecting to GitHub through the API via JSON.

--- batch/create.bat
+++ batch/create.bat
@@ -1,37 +1,30 @@
 :: ENTER PATH WHERE THE script.py IS LOCATED.
 cd C:\Users\David E\version-control\ProjectCreationAutomation
 
-python script.py %1 %2 %~3
+python script.py %1 %2 %3
 
 :: ENTER PATH WHERE YOUR PROJECTS ARE SAVED
 :: e.g. C:\Users\<USERNAME>\Documents\Projects
 cd C:\Users\David E\version-control\%1
 
 
+
+echo "# -" >> README.md
 git init
-
-::ENTER YOUR GIT USERNAME
-git remote rm origin
 git remote add origin https://github.com/mevorahde/%1.git
-
-echo # %1 > README.md
-git add .
-git commit -m "initial commit"
-git push -u origin master
-
-:: NO PARM TO OPEN IDE
-::IF %~3 =="" echo : Finished
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git push -u origin main
 
 :: OPENS PYCHARM
-IF %~3==pycharm (pycharm64 .)
+IF %3==pycharm (pycharm64 .)
 
 :: OPENS VISUAL STUDIO
-IF %~3==visualstudio (devenv.exe .)
+IF %3==visualstudio (devenv.exe .)
 
 :: OPENS SUBLIME
-IF %~3==sublime (subl .)
+IF %3==sublime (subl .)
 
 :: OPENS VISUAL STUDIO CODE
 IF %3==vsc (code .)
-
-
--- script.py
+++ script.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import json
+import requests
 from github import Github
 from dotenv import load_dotenv
 from pathlib import Path
@@ -15,6 +17,9 @@ git_username = os.getenv("username")
 # ENTER YOUR GITHUB PASSWORD
 git_password = os.getenv("password")
 
+# Get Github Token
+token = os.getenv('gt')
+
 # ENTER THE COMPLETE PATH TO YOUR LOCATION WHERE YOU WANT TO SAVE YOUR PROJECTS
 # e.g. C:/Users/<USERNAME>/Documents/Projects/
 ## !!!YOU MUST SEPERATE FOLDERS WITH NORMAL-SLASHES NOT BACK-SLASHES AND AT THE END PUT A SLASH LIKE IN THE EXAMPLE!!!
@@ -27,14 +32,23 @@ def create_folder_and_repo():
     code_ide = str(sys.argv[3])
     os.makedirs(path + folder_name)
 
-    user = Github(git_username, git_password).get_user()
+    GITHUB_URL = "https://api.github.com"
+    headers = {
+        "Authorization": "token " + token,
+        "Accept": "application/vnd.github.v3+json"
+    }
 
-    if public_private == "private":
-        print('PRIVATE')
-        user.create_repo(folder_name, private=True)
-    else:
-        print('PUBLIC')
-        user.create_repo(folder_name)
+    try:
+        if public_private == "private":
+            data = {"name": "{}", "private": "true".format(folder_name)}
+            r = requests.post(GITHUB_URL + "/user/repos", data=json.dumps(data), headers=headers)
+            r.raise_for_status()
+        else:
+            data = {"name": "{}", "private": "false".format(folder_name)}
+            r = requests.post(GITHUB_URL + "/user/repos", data=json.dumps(data), headers=headers)
+            r.raise_for_status()
+    except requests.exceptions.RequestException as err:
+        raise SystemExit(err)
 
     print("Successfully created repository {}".format(folder_name))
 
